79adff1265eba63c863ec623e8007a6c6938c8fb
CrysthelAparicio <crysthel.aparicio@unitec.edu>
2018-05-04 10:05:19 -0600

    probando

---
+++ Triangular.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <math.h>
+
+using namespace std;
+
+//ver si un numero es triangular
+int esTriangular(int num){
+	int sumatoria = 0;
+	
+}
